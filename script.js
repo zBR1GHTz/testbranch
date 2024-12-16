16b6ab786914b8b93436f506723ce746ad752ea7
zBR1GHTz <thit.prompijarn@gmail.com>
2024-12-16 09:48:08 +0700

    new test_v3

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
 console.log("Thit prompijarn");
 console.log("Bright")
+
+const display = document.getElementById('display');
+
+function appendValue(value) {
+    display.value += value;
+}
+
+function clearDisplay() {
+    display.value = '';
+}
+
+function calculate() {
+    try {
+        display.value = eval(display.value);
+    } catch (e) {
+        alert('Invalid Expression');
+        clearDisplay();
+    }
+}
